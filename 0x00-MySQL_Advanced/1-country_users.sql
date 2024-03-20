7e82b4f19d56c27ca561ba39ad13feaa20b0178c
Neivanny1 <nevileneivanny@gmail.com>
2024-03-20 22:21:24 +0000

    sql script that creates a table users with id,name,email and country, enumeration of countries

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,9 @@
 -- script that creates table users
 -- with id,name and email,
 -- country, enumeration of countries: US, CO and TN, never null
-use holberton;
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `name` VARCHAR(255),
-    `country` ENUM('US', 'CO', 'TN') NOT NULL 
-)
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
