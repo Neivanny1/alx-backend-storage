be9e10c4472281b68b2ffd9b729dc80901936fd8
Neivanny1 <nevileneivanny@gmail.com>
2024-03-26 12:46:46 +0000

    Python function that inserts a new document in a collection based on kwargs

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -8,7 +8,7 @@ def list_all(mongo_collection):
     """
     List all documents in the collection mongo_collection
     """
-    documents = mongo_collection.find({})
-    if documents.count_documents({}) == 0:
+    documents = mongo_collection.find()
+    if documents.count() == 0:
         return []
     return documents
