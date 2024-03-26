7cc763e11e98a35649f9e4fa706b89853c8b0d9e
Neivanny1 <nevileneivanny@gmail.com>
2024-03-26 10:54:21 +0000

    Python function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Python function that lists all documents in a collection
+'''
+
+
+def list_all(mongo_collection):
+    documents = mongo_collection.find()
+    if documents.count() == 0:
+        return []
+    return documents
